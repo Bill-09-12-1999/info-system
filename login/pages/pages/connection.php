ddc4c00ad776de7ade33397506a2b03f8978b2e2
Bill-09-12-1999 <billalisdan13@gmail.com>
2022-06-01 00:43:22 +0800

    new

---
+++ login/pages/pages/connection.php
@@ -0,0 +1,13 @@
+<?php
+			// Establishing Connection with Server by passing inputs as a parameter
+
+
+
+ $con = mysqli_connect('localhost','id18870513_burgos','6WdElkqIGnEK-aqF','id18870513_luna') or die(mysqli_error());
+
+
+
+			date_default_timezone_set("Asia/Manila");
+
+
+?>
